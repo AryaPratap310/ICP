29b6947ff9956926e5e394a4fbab763bd7a6faf7
Arya Pratap Singh <offarya1@gmail.com>
2025-09-26 22:19:07 +0530

    question 1st commit

---
+++ ICP/src/Day_1/Find_First_and_Last_Position_of_Element_in_Sorted_Array.java
@@ -0,0 +1,35 @@
+package Day_1;
+
+public class Find_First_and_Last_Position_of_Element_in_Sorted_Array {
+
+	class Solution {
+	    public int[] searchRange(int[] nums, int target) {
+	        int [] arr={-1,-1};
+	        int n=Position(nums,target);
+	        if(n==nums.length || nums[n]!=target ){
+	            return arr;
+	        }    
+	        else{
+	            int m=Position(nums,target+1)-1;
+	            arr[0]=n;
+	            arr[1]=m;
+	            return arr;
+	        }
+	    }
+	    public static int Position(int nums[],int target){
+	        int l=0;
+	        int h=nums.length;
+	        while(l<h){
+	            int mid=l+(h-l)/2;
+	            if(nums[mid]>=target){
+	                h=mid;
+	            }
+	            else {
+	                l=mid+1;
+	            }
+	        }
+	        return l;
+	    }
+	}
+
+}
