b51a9bcf98b537158c49c4de11e184ea26de165d
Arya Pratap Singh <offarya1@gmail.com>
2025-09-27 23:27:52 +0530

    Add files via upload

---
+++ ICP/Day_2/Merge_Sorted_Array.java
@@ -0,0 +1,44 @@
+//https://leetcode.com/problems/merge-sorted-array/
+
+package Day_2;
+public class Merge_Sorted_Array {
+
+	class Solution {
+	    public void merge(int[] nums1, int m, int[] nums2, int n) {
+	        int i=0;
+	        int j=0;
+	        int k=0;
+	        int arr[]=new int[m+n];
+	        while(i<m && j<n){
+	            if(nums1[i]<nums2[j]){
+	                arr[k]=nums1[i];
+	                i++;
+	                k++;
+	            }
+	            else{
+	                arr[k]=nums2[j];
+	                j++;
+	                k++;
+	            }
+	        }
+	        while(i<m){
+	            arr[k]=nums1[i];
+	                i++;
+	                k++;
+	        }
+	        while(j<n){
+	            arr[k]=nums2[j];
+	                j++;
+	                k++;
+	        }
+	        for(int l=0;l<arr.length;l++){
+	            nums1[l]=arr[l];
+	        }
+	        //  for(int q=0;q<arr.length;q++){
+	        //     System.out.print(nums1[q]+" ");
+	        // }
+	        
+	    }
+	}
+
+}
---
+++ ICP/Day_2/Valid_Perfect_Square.java
@@ -0,0 +1,26 @@
+//https://leetcode.com/problems/valid-perfect-square/
+
+package Day_2;
+public class Valid_Perfect_Square {
+
+	class Solution {
+	    public boolean isPerfectSquare(int num) {
+	        long l=1;
+	        long h=num;
+	        while(l<=h){
+	            long m=l+(h-l)/2;
+	            if(m*m==num){
+	                return true;
+	            }
+	            else if(m*m<num){
+	                l=m+1;
+	            }
+	            else{
+	                h=m-1;
+	            }
+	        }
+	        return false;
+	    }
+	}
+
+}
