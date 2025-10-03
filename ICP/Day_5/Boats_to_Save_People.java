486b724efeab07586fa1a2e408d0177608ad04c6
Arya Pratap Singh <offarya1@gmail.com>
2025-10-03 10:20:03 +0530

    Updated Boats_to_Save_People.java
    only the class was present early solution was missing

--- ICP/Day_5/Boats_to_Save_People.java
+++ ICP/Day_5/Boats_to_Save_People.java
@@ -1,5 +1,28 @@
 package Day_5;
-
+import java.util.Arrays;
 public class Boats_to_Save_People {
-
+  class Solution {
+	    public int numRescueBoats(int[] people, int limit) {
+	        Arrays.sort(people);
+	        int boat=0;
+	        int i=0;
+	        int j=people.length-1;
+	        while(i<=j){
+	            if(people[i]+people[j]<=limit){
+	                boat++;
+	                i++;
+	                j--;
+	            }
+	            else{
+	                boat++;
+	                j--;
+	            }
+	        }
+	        return boat;
+	    }
+	}
 }
+//TC:O(NlogN)
+//SC:O(1)
+
+
